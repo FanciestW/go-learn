70cd410bb526fa91e7d70421394e8814165f8914
William Lin <wlin26@yahoo.com>
2021-01-23 12:11:13 -0500

    Added errors to greetings module

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting string for a person's name. (Capitalized function name is Go exported name function)
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name given, return empty string with an error
+	if name == "" {
+		return "", errors.New("Empty Name")
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
