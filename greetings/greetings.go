1967b97e5e081e6a42c3673a15ffd7276523af54
William Lin <wlin26@yahoo.com>
2021-01-23 18:05:28 -0500

    Added error handling and slice usage

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting string for a person's name. (Capitalized function name is Go exported name function)
@@ -11,6 +13,22 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Empty Name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats. (No size is specified so it is a slice)
+	formats := []string{"Hi %v, welcome!", "Great to see you, %v!", "Hello there, %v!"}
+
+	// Return a randomly selected message format by specifying a random
+	// index of the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	var message string
-	message = greetings.Hello("William")
+	var error error
+	message, error = greetings.Hello("William")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 	fmt.Println(quote.Go())
 }
