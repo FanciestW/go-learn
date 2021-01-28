fe41f347c8d8ac82d12926a6321498689380214b
William Lin <wlin26@yahoo.com>
2021-01-27 19:20:22 -0500

    Added median calculation and info output

--- grades/main.go
+++ grades/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	var grades = [...]float32{89, 86, 94, 100, 91, 72, 89, 85, 97, 95, 100, 99}
 	gradeSlice := grades[:]
 	average := getAverage(&gradeSlice)
-	fmt.Println(average)
+	median := getMedian(&gradeSlice)
+	fmt.Printf("Average: %.2f%%\nMedian: %.2f%%\n", average, median)
 }
 
 func getAverage(nums *[]float32) float32 {
@@ -15,4 +19,17 @@ func getAverage(nums *[]float32) float32 {
 		sum += n
 	}
 	return sum / float32(len(*nums))
+}
+
+func getMedian(nums *[]float32) float32 {
+	var sortedNums []float64
+	for _, n := range *nums {
+		sortedNums = append(sortedNums, float64(n))
+	}
+	sort.Float64s(sortedNums)
+	var midIndex int = len(sortedNums) / 2
+	if len(sortedNums)%2 == 0 {
+		return float32((sortedNums[midIndex] + sortedNums[midIndex-1]) / 2)
+	}
+	return float32(sortedNums[midIndex])
 }
