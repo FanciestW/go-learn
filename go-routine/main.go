425659b74cef639a4903b3aae08e9c80fee7e6f0
William Lin <wlin26@yahoo.com>
2021-02-21 12:41:37 -0500

    Added more go routines with another channel

--- go-routine/main.go
+++ go-routine/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+)
+
+const (
+	one = iota
 )
 
 // A Go routine function to say hello world
 func hello(done chan bool) {
 	fmt.Println("Hello world")
+	var nameChan chan string = make(chan string)
+	go getName(nameChan)
+	receivedName := <-nameChan
+	fmt.Printf("Welcome, %s\n", receivedName)
 	done <- true
+}
+
+func getName(name chan<- string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Your name: ")
+	text, _ := reader.ReadString('\n')
+	name <- text
 }
 
 func main() {
